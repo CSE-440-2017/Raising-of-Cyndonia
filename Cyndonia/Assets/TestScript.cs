215f568fe9e46adad0df903f380c2938c202b380
Swordsroad <samuraikenny@gmail.com>
2017-10-12 13:58:26 -0700

    Test Push Script
    Kenny made a test script to make sure that he can push to the repository

---
+++ Cyndonia/Assets/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+	/*This is a test script for the purpose of Kenny making sure he can push to the repository
+	 * 
+	 * 
+	 * 
+	 * /
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
