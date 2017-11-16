d6888f08e819804ab75f0847bd79c21df4a9e48f
Raigoth <32273212+Raigoth@users.noreply.github.com>
2017-11-16 13:21:54 -0800

    Added extra scene and started Battle State script

---
+++ Raising Of Cyndonia/Assets/Scripts/BattleStateManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleStateManager : MonoBehaviour 
+{
+	public StatesOfBattle curState;
+
+	// Use this for initialization
+	void Start() 
+	{
+		curState = StatesOfBattle.Start;
+		
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		switch (curState)
+		{
+			case (StatesOfBattle.Start):
+				break;
+			case(StatesOfBattle.PlayerTurn):
+				break;
+				
+		}
+	}
+}
+
+public enum StatesOfBattle
+{
+	//Later will add animations here as well if we have time
+	Start, PlayerTurn, EnemyTurn, Lose, Win
+}
