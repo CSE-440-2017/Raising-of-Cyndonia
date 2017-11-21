f29433f09d1d1a8570756b370db12ba1e91ed4e7
HKProductions <hkproductions9@gmail.com>
2017-11-21 12:44:21 -0800

    Commented the attack component to make it more readable

--- Raising of Cyndonia/Assets/Scripts/AttackComponent.cs
+++ Raising of Cyndonia/Assets/Scripts/AttackComponent.cs
@@ -30,68 +30,68 @@ public class AttackComponent : MonoBehaviour {
 		float dmgPercentage = dmgPercent / 100;
 
 		//See if the attack the entity chose is a weakness of the entity they are attacking
-		if (other.GetComponent<Entity> ().Weakness.ToString () == bUI.magicType.ToString ()) {
+		if (other.GetComponent<Entity> ().Weakness.ToString () == entInfo.Magic.ToString ()) {
 			//See if the attack is a direct attack then increase the base damage by 1.5 and then attack the entity 
-			if (bUI.attackType.ToString () == "Direct") {
-				baseDMG *= 1.5f;
-				updatedDMG = baseDMG * dmgPercentage;
-				totalDMG = (int)baseDMG - (int)updatedDMG;
+			if (entInfo.Attacks.ToString () == "Melee") {
+				baseDMG *= 1.5f; //Calculates how much to increase the base damage to
+				updatedDMG = baseDMG * dmgPercentage;//See's how effective the damage will be 
+				totalDMG = (int)baseDMG - (int)updatedDMG;//Subtracts the base damage with the updated damge to give proper damage value
 				other.GetComponent<HealthComponent> ().HealthDamaged (totalDMG);
 			} 
 
 			//See fi the attack is a magic attack then increase the magic damage by 1.5 and then attack the entity
-			else if (bUI.attackType.ToString () == "Magic") {
-				magicDMG *= 1.5f;
-				updatedDMG = magicDMG * dmgPercentage;
-				totalDMG = (int)magicDMG - (int)updatedDMG;
+			else if (entInfo.Attacks.ToString () == "Magic") {
+				magicDMG *= 1.5f;//Calculates how much to increase the base magic damage to
+				updatedDMG = magicDMG * dmgPercentage;//Calculates how much to increase the base magic damage to 
+				totalDMG = (int)magicDMG - (int)updatedDMG;//Subtracts the base damage with the updated damge to give proper damage value
 				other.GetComponent<HealthComponent> ().HealthDamaged (totalDMG);
 			}
 		} 
 
 		//See if the attack the entity chose is not as effective to the entity they are attacking
 		else if (other.GetComponent<Entity> ().Invulnerable.ToString () == bUI.magicType.ToString ()) {
-			if (bUI.attackType.ToString () == "Direct") {
-				baseDMG /= 1.5f;
-				updatedDMG = baseDMG * dmgPercentage;
-				totalDMG = (int)baseDMG - (int)updatedDMG;
+			if (entInfo.Attacks.ToString () == "Direct") {
+				baseDMG /= 1.5f; //Calculates how much to decrease the base damage 
+				updatedDMG = baseDMG * dmgPercentage; //See how effective the damage will be
+				totalDMG = (int)baseDMG - (int)updatedDMG; //Subtract the base damage from the updated damage to give proper value
 				other.GetComponent<HealthComponent> ().HealthDamaged (totalDMG);
 			} 
 
-			else if (bUI.attackType.ToString () == "Magic") {
-				magicDMG /= 1.5f;
-				updatedDMG = magicDMG * dmgPercentage;
-				totalDMG = (int)magicDMG - (int)updatedDMG;
+			else if (entInfo.Attacks.ToString () == "Magic") {
+				magicDMG /= 1.5f;//Calculates how much to decrease the base magic damage 
+				updatedDMG = magicDMG * dmgPercentage;//See how effective the damage will be
+				totalDMG = (int)magicDMG - (int)updatedDMG;//Subtracts the base damage with the updated damge to give proper damage value
 				other.GetComponent<HealthComponent> ().HealthDamaged (totalDMG);
 			}
 		}
 
 		//See if the entity is a knight and using a direct attack and as a result the base attack is boosted
-		else if (bUI.attackType.ToString () == "Direct" && entInfo.Role.ToString () == "Knight") {
-			baseDMG *= 1.5f;
-			updatedDMG = baseDMG * dmgPercentage;
-			totalDMG = (int)baseDMG - (int)updatedDMG;
+		else if (entInfo.Attacks.ToString () == "Direct" && entInfo.Role.ToString () == "Knight") {
+			baseDMG *= 1.5f;//Calculates how much to increase the base damage by
+			updatedDMG = baseDMG * dmgPercentage;//See how effective the damage will be
+			totalDMG = (int)baseDMG - (int)updatedDMG;//Subtracts the base damage with the updated damge to give proper damage value
 			other.GetComponent<HealthComponent>().HealthDamaged (totalDMG);
 		} 
 
 		//See if the entity is a mage and using a magic attack and as a result the magic attack is boosted
-		else if (bUI.attackType.ToString () == "Magic" && entInfo.Role.ToString () == "Mage") {
-			magicDMG *= 1.5f;
-			updatedDMG = magicDMG * dmgPercentage;
-			totalDMG = (int)magicDMG - (int)updatedDMG;
+		else if (entInfo.Attacks.ToString () == "Magic" && entInfo.Role.ToString () == "Mage") {
+			magicDMG *= 1.5f;//Calculates how much to increase the base magic damage by 
+			updatedDMG = magicDMG * dmgPercentage;//See how effective the damage will be
+			totalDMG = (int)magicDMG - (int)updatedDMG;//Subtracts the base damage with the updated damge to give proper damage value
 			other.GetComponent<HealthComponent>().HealthDamaged (totalDMG);
 		}
 
 		//See if the attack is just a direct attack so just do the attack
-		else if (bUI.attackType.ToString () == "Direct") {
-			updatedDMG = baseDMG * dmgPercentage;
-			totalDMG = (int)baseDMG - (int)updatedDMG;
+		else if (entInfo.Attacks.ToString () == "Direct") {
+			updatedDMG = baseDMG * dmgPercentage;//See how effective the damage will be
+			totalDMG = (int)baseDMG - (int)updatedDMG;//Subtracts the base damage with the updated damge to give proper damage value
 			other.GetComponent<HealthComponent>().HealthDamaged (totalDMG);
 		} 
 
 		//See if the attack is a magic attack and if so then just do the attack
-		else if (bUI.attackType.ToString () == "Magic") {
-			updatedDMG = magicDMG * dmgPercentage;
-			totalDMG = (int)magicDMG - (int)updatedDMG;
+		else if (entInfo.Attacks.ToString () == "Magic") {
+			updatedDMG = magicDMG * dmgPercentage;//See how effective the damage will be
+			totalDMG = (int)magicDMG - (int)updatedDMG;//Subtracts the base damage with the updated damge to give proper damage value
 			other.GetComponent<HealthComponent>().HealthDamaged (totalDMG);
 		}
 	}
